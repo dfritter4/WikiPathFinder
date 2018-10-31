2580bbe60af0fc83e5856e4173479f56d90db088
David Fritz (dev) <fritz_development@Davids-MacBook-Pro-3.local>
2018-10-30 20:03:02 -0500

    initial commit for bento philosophy wiki path finder application

---
+++ src/main/java/com/fritz/philsofinder/PhilsofinderApplication.java
@@ -0,0 +1,12 @@
+package com.fritz.philsofinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhilsofinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhilsofinderApplication.class, args);
+	}
+}
